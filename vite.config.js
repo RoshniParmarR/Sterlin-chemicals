d39886cb76fbf9d8ffe49c7603ea7046565b35e5
RoshniParmarR <roshniparmar1123@gmail.com>
2026-08-09 14:00:06 +0530

    third commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Sterlin-chemicals/',
   plugins: [react()],
   resolve: {
     alias: {
@@ -13,4 +14,8 @@ export default defineConfig({
   server: {
     port: 5174,
   },
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
 })
+
